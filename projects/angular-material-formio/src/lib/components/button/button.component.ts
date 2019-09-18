e6b01902d8bbc00d34aeb699d061a18d22f7e6b2
Travis Tidwell <travis@form.io>
2019-09-18 11:17:49 -0500

    Fixing the button icon to communicate form submission states.

--- projects/angular-material-formio/src/lib/components/button/button.component.ts
+++ projects/angular-material-formio/src/lib/components/button/button.component.ts
@@ -4,14 +4,49 @@ import { MaterialComponent } from '../MaterialComponent';
 @Component({
   selector: 'mat-formio-button',
   template: `
-    <button
-      type="{{ instance.component.action }}"
-      mat-raised-button color="{{ instance.component.theme || 'primary' }}"
-      (click)="instance.onClick($event)">{{ instance.component.label }}
+    <button type="{{ instance.component.action }}" mat-raised-button [color]="color" (click)="instance.onClick($event)">
+      <mat-icon *ngIf="done">done</mat-icon>
+      <mat-icon *ngIf="error">close</mat-icon>
+      <mat-icon class="mat-icon-spin" *ngIf="loading">autorenew</mat-icon>
+      {{ instance.component.label }}
     </button>
   `,
-  styles: []
+  styles: ['@keyframes spin { from { transform:rotate(0deg); } to { transform:rotate(360deg); } } ' +
+    ':host .mat-icon-spin { ' +
+      'animation-name: spin; ' +
+      'animation-duration: 1000ms; ' +
+      'animation-iteration-count: infinite; ' +
+      'animation-timing-function: linear; ' +
+    '}'
+  ]
 })
-export class MaterialButtonComponent extends MaterialComponent {}
+export class MaterialButtonComponent extends MaterialComponent {
+  public loading = false;
+  public done = false;
+  public error = false;
+  public color = 'primary';
+  getColor() {
+    if (this.error) {
+      return 'warn';
+    }
+    return this.instance.component.theme || 'primary';
+  }
+  setState(loading, error, done) {
+    this.loading = loading;
+    this.done = done;
+    this.error = error;
+    this.color = this.getColor();
+  }
+  setInstance(instance) {
+    const retVal = super.setInstance(instance);
+    this.color = this.getColor();
+    instance.on('submitButton', () => this.setState(true, false, false));
+    instance.on('submitDone', () => this.setState(false, false, true));
+    instance.on('submitError', () => this.setState(false, true, false));
+    instance.on('requestButton', () => this.setState(true, false, false));
+    instance.on('requestDone', () => this.setState(false, false, true));
+    return retVal;
+  }
+}
 ButtonComponent.MaterialComponent = MaterialButtonComponent;
 export { ButtonComponent };
