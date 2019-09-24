ec7ddf2810f3cf95461971bc30adfac71a1c7008
Travis Tidwell <travis@form.io>
2019-09-24 16:08:27 -0500

    Fixing custom component.

--- demo/src/app/CustomComponent.ts
+++ demo/src/app/CustomComponent.ts
@@ -1,13 +1,13 @@
 /**
  * This file demonstrates how to create a custom component in Angular Material
  */
-import { Components, MaterialComponent } from 'angular-material-formio';
+import { Components, MaterialHtmlComponent } from 'angular-material-formio';
 import HtmlComponent from 'formiojs/components/html/HTML.js';
 class HeaderComponent extends HtmlComponent {
   // Custom logic goes here.
 }
-export class MaterialHeaderComponent extends MaterialComponent {
+export class MaterialHeaderComponent extends MaterialHtmlComponent {
   // Custom Material logic goes here.
 }
-HeaderComponent.MaterialComponent = MaterialHeaderComponent;
+(HeaderComponent as any).MaterialComponent = MaterialHeaderComponent;
 Components.addComponent('header', HeaderComponent);
