97ccfe75c988c03b342b62e815fa16322d6a7f1a
Travis Tidwell <travis@form.io>
2019-11-13 19:59:37 -0600

    Adding expandable panels.

--- projects/angular-material-formio/src/lib/components/panel/panel.component.ts
+++ projects/angular-material-formio/src/lib/components/panel/panel.component.ts
@@ -4,12 +4,20 @@ import PanelComponent from 'formiojs/components/panel/Panel.js';
 @Component({
   selector: 'mat-formio-panel',
   template: `
-    <mat-card>
+    <mat-card *ngIf="!instance.component.collapsible">
       <mat-card-title *ngIf="instance?.component?.title">{{ instance.component.title }}</mat-card-title>
       <mat-card-content fxLayout="column" fxLayoutGap="1em">
         <ng-template #components></ng-template>
       </mat-card-content>
     </mat-card>
+    <mat-expansion-panel *ngIf="instance.component.collapsible" [expanded]="!instance.component.collapsed">
+      <mat-expansion-panel-header *ngIf="instance?.component?.title">
+        <mat-panel-title>
+          {{ instance.component.title }}
+        </mat-panel-title>
+      </mat-expansion-panel-header>
+      <ng-template #components></ng-template>
+    </mat-expansion-panel>
   `,
   styles: []
 })
