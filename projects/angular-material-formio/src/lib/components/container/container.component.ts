f1fdc0a2dc407abe3d61ecd5112acf63802bb9a0
Vlad <vlad.shusterman30@gmail.com>
2020-02-17 13:09:02 +0300

    Fix(Container): add label & hideLabel & labelPosition

--- projects/angular-material-formio/src/lib/components/container/container.component.ts
+++ projects/angular-material-formio/src/lib/components/container/container.component.ts
@@ -3,8 +3,24 @@ import { MaterialNestedComponent } from '../MaterialNestedComponent';
 import ContainerComponent from 'formiojs/components/container/Container.js';
 @Component({
   selector: 'mat-formio-container',
-  template: `<div fxLayout="column" fxLayoutGap="1em"><ng-template #components></ng-template></div>`
+  template: `
+    <div fxLayout="column" fxLayoutGap="1em">
+      <mat-label *ngIf="getLabelPosition() === 'top'">
+        {{ instance.component.label }}
+      </mat-label>
+      <ng-template #components></ng-template>
+      <mat-label *ngIf="getLabelPosition() === 'bottom'">
+        {{ instance.component.label }}
+      </mat-label>
+    </div>`
 })
-export class MaterialContainerComponent extends MaterialNestedComponent {}
+export class MaterialContainerComponent extends MaterialNestedComponent {
+  getLabelPosition() {
+    if (this.instance.component && !this.instance.component.hideLabel && this.instance.component.label) {
+      return this.instance.component.labelPosition === 'bottom' ? 'bottom' : 'top';
+    }
+    return null;
+  }
+}
 ContainerComponent.MaterialComponent = MaterialContainerComponent;
 export { ContainerComponent };
