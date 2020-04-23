b482a4c314da7123bedd4b6ca9296f256fe04bf8
mikekotikov <mikhail@form.io>
2020-04-23 12:37:04 +0300

    Feat (TextArea): add prefix and suffix

--- projects/angular-material-formio/src/lib/components/textarea/textarea.component.ts
+++ projects/angular-material-formio/src/lib/components/textarea/textarea.component.ts
@@ -1,13 +1,15 @@
 import { Component, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
 import { MaterialComponent } from '../MaterialComponent';
 import TextAreaComponent from 'formiojs/components/textarea/TextArea.js';
-import _ from 'lodash';
+import isNil from 'lodash/isNil';
 
 @Component({
   selector: 'mat-formio-textarea',
+  styleUrls: ['./textarea.component.css'],
   template: `
-    <mat-form-field class="mat-formio-textarea" [ngClass]="{'editor-enabled': !!instance.component.editor}" fxFill>
+    <mat-form-field class="mat-formio-textarea" [ngClass]="{'editor-enabled': !!instance.component.editor}" fxFill fxFlexAlign="center">
       <mat-label fxFill>{{ instance.component.label }}</mat-label>
+      <span *ngIf="instance.component.prefix" matPrefix>{{ instance.component.prefix }}&nbsp;</span>
       <textarea matInput
           [required]="instance.component.validate?.required"
           [placeholder]="instance.component.placeholder"
@@ -15,6 +17,7 @@ import _ from 'lodash';
           [rows]="(instance.component.rows || 3)"
           (input)="onChange()" #textarea>
       </textarea>
+      <span *ngIf="instance.component.suffix" matSuffix>{{ instance.component.suffix }}</span>
       <mat-icon *ngIf="instance.component.tooltip" matSuffix matTooltip="{{ instance.component.tooltip }}">info</mat-icon>
       <mat-hint *ngIf="instance.component.description">{{ instance.component.description }}</mat-hint>
       <mat-error *ngIf="instance.error">{{ instance.error.message }}</mat-error>
@@ -29,7 +32,7 @@ export class MaterialTextareaComponent extends MaterialComponent implements Afte
   }
 
   getValue() {
-    return _.isNil(this.control.value) ? '' : this.control.value;
+    return isNil(this.control.value) ? '' : this.control.value;
   }
 }
 TextAreaComponent.MaterialComponent = MaterialTextareaComponent;
