be64ceeffadff9e7bbedc75f99c8c19dc851c51b
Albert Gracia <Albert.Gracia@avaloq.com>
2020-04-30 13:15:15 +0200

    @angular/material migration - migration-v9 Updates Angular Material to v9

--- demo/src/app/app.module.ts
+++ demo/src/app/app.module.ts
@@ -3,7 +3,6 @@ import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
 import { MatFormioModule } from 'angular-material-formio';
-import 'hammerjs';
 import {MaterialHeaderComponent} from './CustomComponent';
 
 @NgModule({
