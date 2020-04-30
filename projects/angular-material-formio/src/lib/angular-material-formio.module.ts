e27fb19e6a1dc2a2c70b626d11993ac76e1390bb
Albert Gracia <Albert.Gracia@avaloq.com>
2020-04-30 11:30:44 +0200

    @angular/material migration - migration-v9 Updates Angular Material to v9

--- projects/angular-material-formio/src/lib/angular-material-formio.module.ts
+++ projects/angular-material-formio/src/lib/angular-material-formio.module.ts
@@ -2,26 +2,25 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { FlexLayoutModule } from '@angular/flex-layout';
-import {
-  MatInputModule,
-  MatButtonModule,
-  MatFormFieldModule,
-  MatCheckboxModule,
-  MatRadioModule,
-  MatSelectModule,
-  MatListModule,
-  MatChipsModule,
-  MatExpansionModule,
-  MatCardModule,
-  MatStepperModule,
-  MatTabsModule,
-  MatTableModule,
-  MatDatepickerModule,
-  MatNativeDateModule,
-  MatProgressSpinnerModule,
-  MatIconModule,
-  MatTooltipModule, MatMenuModule
-} from '@angular/material';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatListModule } from '@angular/material/list';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatSelectModule } from '@angular/material/select';
+import { MatStepperModule } from '@angular/material/stepper';
+import { MatTableModule } from '@angular/material/table';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 import { initRenderer } from './renderer';
 import { FormioComponent } from './formio.component';
