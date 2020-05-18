18c9d25749173985f579c6265bdc68dab7ffb975
mikekotikov <mikhail@form.io>
2020-05-18 21:21:41 +0300

    Fix (Demo): change demo polyfills to have submission data being set every time

--- demo/src/polyfills.ts
+++ demo/src/polyfills.ts
@@ -18,16 +18,27 @@
  * BROWSER POLYFILLS
  */
 
-/** IE10 and IE11 requires the following for NgClass support on SVG elements */
-// import 'classlist.js';  // Run `npm install --save classlist.js`.
+if (!String.prototype.includes) {
+    String.prototype.includes = function (search: any, start) {
+        'use strict';
 
+        if (search instanceof RegExp) {
+            throw TypeError('first argument must not be a RegExp');
+        }
+        if (start === undefined) { start = 0; }
+        return this.indexOf(search, start) !== -1;
+    };
+}
+
+/** IE10 and IE11 requires the following for NgClass support on SVG elements */
+import 'classlist.js'; // Run `npm install --save classlist.js`.
+import 'core-js';
 /**
  * Web Animations `@angular/platform-browser/animations`
  * Only required if AnimationBuilder is used within the application and using IE/Edge or Safari.
  * Standard animation support in Angular DOES NOT require any polyfills (as of Angular 6.0).
  */
 // import 'web-animations-js';  // Run `npm install --save web-animations-js`.
-
 /**
  * By default, zone.js will patch all possible macroTask and DomEvents
  * user can disable parts of macroTask/DomEvents patch by setting following flags
@@ -51,14 +62,12 @@
  *  (window as any).__Zone_enable_cross_context_check = true;
  *
  */
-
 /***************************************************************************************************
  * Zone JS is required by default for Angular itself.
  */
-import 'zone.js/dist/zone';  // Included with Angular CLI.
-
-
 /***************************************************************************************************
  * APPLICATION IMPORTS
  */
-(window as any).global = window;
+import 'hammerjs/hammer';
+import 'zone.js/dist/zone'; // Included with Angular CLI.
+
